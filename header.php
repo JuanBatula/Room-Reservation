c9ac0e1eef8b03d5049eac289339dbf70a8d870d
JuanBatula <wanifywanify@gmail.com>
2026-05-05 00:43:07 +0800

    feat: changed the header file that fits into our own project

--- header.php
+++ header.php
@@ -7,10 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	  <link href = "https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap.min.css">	
     <link rel="stylesheet" href="styles/header.css">
 
     <link rel = "stylesheet" href = "css/site.css"/>
@@ -19,16 +15,29 @@
   </head>
   <body>
 
-  	<div class = "container">
+    <header class="container">
+      <img src="images/citlogo.png" class="cit-logo">
+    </header>
+
+
+
+
+
+
+
+
+
+
+  	<!-- <div class = "container">
   		<nav class="navbar navbar-expand-lg navbar-light bg-light">
   	<a class="navbar-brand" href="index.php">
   		<img src="images/logosrp.png" width="50" height="40">
   	</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
@@ -50,14 +59,14 @@
  	      <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
-      </li>
+      </li> -->
       <!-- <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li> -->
-    </ul>
+    <!-- </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-  </div>
+  </div> -->
 </nav>
