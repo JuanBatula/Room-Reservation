cfee7c78b9c2825e500db3da29c1e548f933ef0e
JuanBatula <wanifywanify@gmail.com>
2026-05-16 20:46:38 +0800

    feat: added styling for the edit users page

--- admin/users.php
+++ admin/users.php
@@ -2,7 +2,6 @@
 include '../connect.php';
 include '../includes/header.php';
 
-
 $result = mysqli_query($connection, "SELECT * FROM tbuser");
 
 /*
@@ -10,27 +9,53 @@ $result = mysqli_query($connection, "SELECT u.* FROM tbuser u
           INNER JOIN tbstudent s ON u.user_id = s.user_id");
 */
 ?>
-<h2>Users</h2>
 
-<table border="1">
-    <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Action</th>
-    </tr>
-    <?php while($row=mysqli_fetch_assoc($result)){ ?>
-    <tr>
-        <td>
-        <?php
-        echo $row['first_name']." ".$row['last_name'];
-        ?>
-        </td>
-        <td><?php echo $row['email']; ?></td>
-        <td>
-        <a href="edit_user.php?id=<?php echo $row['user_id']; ?>">Edit</a>|
-        <a href="delete_user.php?id=<?php echo $row['user_id']; ?>"
-        onclick="return confirm('Are you sure you want to delete this user?')">Delete</a>
-        </td>
-    </tr>
-    <?php } ?>
-</table>
+<link rel="stylesheet" href="../styles/users.css">
+
+<div class="topbar">
+    <div class="topbar-left">
+        <div class="topbar-accent"></div>
+        <h1>User Management</h1>
+    </div>
+</div>
+<div class="gold-bar"></div>
+
+<div class="page">
+
+    <div class="table-wrap">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($row = mysqli_fetch_assoc($result)){ ?>
+                <tr>
+                    <td><span class="id-pill">#<?php echo $row['user_id']; ?></span></td>
+                    <td>
+                        <div class="user-name"><?php echo htmlspecialchars($row['first_name'] . ' ' . $row['last_name']); ?></div>
+                    </td>
+                    <td>
+                        <div class="user-email"><?php echo htmlspecialchars($row['email']); ?></div>
+                    </td>
+                    <td>
+                        <div class="actions">
+                            <a href="edit_user.php?id=<?php echo $row['user_id']; ?>" class="btn-edit">Edit</a>
+                            <a href="delete_user.php?id=<?php echo $row['user_id']; ?>"
+                               class="btn-del"
+                               onclick="return confirm('Are you sure you want to delete this user?')">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+<?php include '../includes/footer.php'; ?>
